f15aa0a947159bdac7f85a683347ab47e2dfd9b3
Daniel Micay <danielmicay@gmail.com>
2021-11-02 14:44:13 -0400

    invalidate icon cache between OS releases
    Change-Id: If33a3d5ffa5b2c6cbb15e4a46a819a5dfb2efa1f
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- iconloaderlib/src/com/android/launcher3/icons/IconProvider.java
+++ iconloaderlib/src/com/android/launcher3/icons/IconProvider.java
@@ -266,7 +266,7 @@ public class IconProvider {
     public void updateSystemState() {
         mSystemState = mSystemState.withLocaleAndSdk(
                 mContext.getResources().getConfiguration().getLocales().toLanguageTags(),
-                Build.VERSION.SDK_INT);
+                Build.VERSION.INCREMENTAL);
     }
 
     /**
